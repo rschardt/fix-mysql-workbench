78b6ac17190e67e86db4db3ab09d63b2dd282cad
Robert Schardt <robert.schardt@iohk.io>
2022-11-17 17:43:59 +0100

    Add flake for mysql-workbench from unstable nixpkgs.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake for someone using mysql-workbench";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [pkgs.mysql-workbench];
+      };
+  });
+}
